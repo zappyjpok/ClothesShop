93e02988e319d6deb06e4a45b60828d0a169dd51
zappyjpok <potentia2020@yahoo.com>
2015-09-04 22:09:39 +1000

    Library added included files for uploading and deleting files and the shopping cart.  Also, gulpfile.js now concats javascript files

--- gulpfile.js
+++ gulpfile.js
@@ -1,2 +1,14 @@
 var gulp = require('gulp'),
-    gutil = require('gulp-util');
+    gutil = require('gulp-util'),
+    concat = require('gulp-concat');
+
+var jsSources = [
+    'App/Resources/js/validation.js',
+    'App/Resources/js/main.js'
+];
+
+gulp.task('js', function(){
+   gulp.src(jsSources)
+       .pipe(concat('all.js'))
+       .pipe(gulp.dest('Public/JS'))
+});
